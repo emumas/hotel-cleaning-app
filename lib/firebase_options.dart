988287bad6f7da2f15cf5c6eed2a9631a0551d4e
emumas <82310012+emumas@users.noreply.github.com>
2026-05-04 18:47:45 +0900

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,3 +1,8 @@
+@@ -1,43 +1,25 @@
+// このファイルは FlutterFire CLI で自動生成されます
+// flutter pub global activate flutterfire_cli
+// flutterfire configure
+// を実行してください
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -5,6 +10,24 @@ import 'package:hotel_cleaning_app/app.dart';
 import 'package:hotel_cleaning_app/firebase_options.dart';
 import 'package:hotel_cleaning_app/providers/providers.dart';
 
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError('Web is not supported');
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      default:
+        throw UnsupportedError(
+            'DefaultFirebaseOptions are not supported for this platform.');
+    }
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
@@ -17,6 +40,22 @@ void main() async {
     debugPrint('Firebase initialization error: $e');
   }
 
+  // TODO: flutterfire configure を実行してこの値を自動生成してください
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'YOUR_ANDROID_API_KEY',
+    appId: 'YOUR_ANDROID_APP_ID',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'YOUR_IOS_API_KEY',
+    appId: 'YOUR_IOS_APP_ID',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    iosBundleId: 'com.example.hotelCleaningApp',
   runApp(
     const ProviderScope(
       child: HotelCleaningApp(), // _AppInitを介さず直接起動して型エラーを回避
