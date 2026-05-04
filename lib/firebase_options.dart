e3c7dcfa1d42cb04b18868c2c79153849efc575d
emumas <82310012+emumas@users.noreply.github.com>
2026-05-04 19:03:00 +0900

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,15 +1,3 @@
-@@ -1,43 +1,25 @@
-// このファイルは FlutterFire CLI で自動生成されます
-// flutter pub global activate flutterfire_cli
-// flutterfire configure
-// を実行してください
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hotel_cleaning_app/app.dart';
-import 'package:hotel_cleaning_app/firebase_options.dart';
-import 'package:hotel_cleaning_app/providers/providers.dart';
-
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
@@ -17,7 +5,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError('Web is not supported');
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -28,37 +16,32 @@ class DefaultFirebaseOptions {
         throw UnsupportedError(
             'DefaultFirebaseOptions are not supported for this platform.');
     }
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  // Web環境での初期化をより確実にする設定
-  try {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-  } catch (e) {
-    debugPrint('Firebase initialization error: $e');
   }
 
-  // TODO: flutterfire configure を実行してこの値を自動生成してください
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBHqgce--zPg0wrzBEqkz0tQMx2zBpaYEw',
+    appId: '1:851039599507:web:348858f3401d2893fb0666',
+    messagingSenderId: '851039599507',
+    projectId: 'hotel-47826',
+    authDomain: 'hotel-47826.firebaseapp.com',
+    storageBucket: 'hotel-47826.firebasestorage.app',
+    measurementId: 'G-ETSJK1SHFK',
+  );
+
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'YOUR_ANDROID_API_KEY',
     appId: 'YOUR_ANDROID_APP_ID',
     messagingSenderId: 'YOUR_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    projectId: 'hotel-47826',
+    storageBucket: 'hotel-47826.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'YOUR_IOS_API_KEY',
     appId: 'YOUR_IOS_APP_ID',
     messagingSenderId: 'YOUR_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    projectId: 'hotel-47826',
+    storageBucket: 'hotel-47826.firebasestorage.app',
     iosBundleId: 'com.example.hotelCleaningApp',
-  runApp(
-    const ProviderScope(
-      child: HotelCleaningApp(), // _AppInitを介さず直接起動して型エラーを回避
-    ),
   );
 }
